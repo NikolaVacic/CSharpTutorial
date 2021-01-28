b417072b43ab118f476e97f75bca7d7178436c15
Nikola Vacic <nikolavacic23@gmail.com>
2021-01-28 22:22:35 +0100

    Type Conversion and Conditionals

---
+++ Syntax/Conditionals/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conditionals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // if, else, else if, && i || je isto kao u C++.
+
+            Console.Write("Unesi broj meseca u kojem si rodjen : ");
+            
+            int month;
+
+            month = Convert.ToInt32(Console.ReadLine()); // ovako se mora inputovati int
+ 
+            switch (month)
+            {
+                case 1: Console.WriteLine("Januar"); break;
+                case 2: Console.WriteLine("Februar"); break;
+                case 3: Console.WriteLine("Mart"); break;
+                case 4: Console.WriteLine("April"); break;
+                case 5: Console.WriteLine("Maj"); break;
+                case 6: Console.WriteLine("Jun"); break;
+                case 7: Console.WriteLine("Jul"); break;
+                case 8: Console.WriteLine("Avgust"); break;
+                case 9: Console.WriteLine("Septembar"); break;
+                case 10: Console.WriteLine("Oktobar"); break;
+                case 11: Console.WriteLine("Novembar"); break;
+                case 12: Console.WriteLine("Decembar"); break;
+                default: Console.WriteLine("Unesi broj iz intervala [1,12]"); break;
+            }
+
+
+        }
+    }
+}
---
+++ Syntax/TypeConversion/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Koliko imas godina: ");
+
+            string age = Console.ReadLine();
+
+            Console.WriteLine($"Za 5 godina ces imati { age + 5} godina");
+
+
+            // Postoji bezbedan i nebezbedan nacin i zavisi od poverenja u input korisnika.
+
+            // Ako verujemo korisniku:
+
+            // problem sa ovim pristupom je taj sto ako ukucamo npr 'dvadeset' slovima dobicemo exception
+
+            int godineNebezbedno = int.Parse(age);
+
+            Console.WriteLine($"Za 5 godina ces imati { godineNebezbedno + 5 } godina");
+
+
+            // bezbedan nacin
+
+            bool validneGodine = int.TryParse(age, out int godineBezbedno);
+
+            if (validneGodine)
+                Console.WriteLine($"Za 5 godina ces imati { godineBezbedno + 5 } godina");
+            else
+                Console.WriteLine("Unesi godine u brojevima");
+
+            // Mantra: Nikad ne veruj korisniku da ce uneti ispravne podatke.
+
+            // tryParse postoji za (verovatno) svaki tip podataka.
+
+            Console.WriteLine("Bool parse primer, da li si stariji od 20 godina? ");
+            string odgovor = "";
+
+            odgovor = Console.ReadLine();
+
+            bool validanOdgovor = bool.TryParse(odgovor, out bool odg);
+
+            if(validanOdgovor)
+                Console.WriteLine(odg);
+            else
+                Console.WriteLine("Odgovori sa true/false ");
+
+        }
+    }
+}
